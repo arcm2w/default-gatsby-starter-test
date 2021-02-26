7a98b52fbfddab0a1b427660c95e4b4cd45c1c55
arcm2w <77464536+arcm2w@users.noreply.github.com>
2021-02-26 22:47:35 +0000

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi Maluk sira</h1>
+    <p>Welcome mai hau nia sites primeiro.</p>
+    <p>Hau sei aprende hela, no sei esforco atu hadiak diak liu tan hau nia sites.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
